2efa89de5b077c79a5806e4ee908036dc1ce4527
EmmanuelSan01 <emmanuel.sanchez.estepa@gmail.com>
2026-09-15 18:59:28 -0500

    Primer commit: estructura inicial del proyecto

---
+++ src/main/java/co/edu/unab/micompu/MicompuApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unab.micompu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicompuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicompuApplication.class, args);
+	}
+
+}
